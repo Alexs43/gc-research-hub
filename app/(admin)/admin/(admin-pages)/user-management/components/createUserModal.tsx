3dc933ee3b4733a63b4515707688005187e4fc3c
dardeejay <dardeejay.calura28@gmail.com>
2023-12-06 17:23:20 +0800

    Fix function declaration and add label for email input

--- app/(admin)/admin/(admin-pages)/user-management/components/createUserModal.tsx
+++ app/(admin)/admin/(admin-pages)/user-management/components/createUserModal.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes, faSave } from "@fortawesome/free-solid-svg-icons";
-export default function CreateUserModal() {
+export default  function  CreateUserModal() {
+  async function create(formData: FormData){
+    "use server";
+
+  }
   return (
     <div className="h-screen w-screen flex justify-center items-center fixed top-0 right-0 bg-[rgb(0,0,0,0.3)] z-30 ">
       <div className="w-1/2 bg-white rounded-xl py-3 px-4">
@@ -14,7 +18,8 @@ export default function CreateUserModal() {
         </div>
         <hr className="border-primaryGreen border" />
         <form action="">
-            
+            <label htmlFor="">Email</label>
+            {/* first name, middle name, last name, email, contact num, dob, gender, role, college */}
         </form>
       </div>
     </div>
